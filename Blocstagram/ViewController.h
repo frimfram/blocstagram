2d1c5ac58f4f2827179635bca917fb52d1d08dda
Jean Ro <jsro88@gmail.com>
2014-11-23 20:16:44 -0800

    Initial Commit

---
+++ Blocstagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Blocstagram
+//
+//  Created by Jean Ro on 11/23/14.
+//  Copyright (c) 2014 Jean Ro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Blocstagram/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Blocstagram
+//
+//  Created by Jean Ro on 11/23/14.
+//  Copyright (c) 2014 Jean Ro. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
